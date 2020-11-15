e40650a3d5d08ecaf423625f2467c9a4eb9fe5bc
ParkJeongHoon <kangtak6291@naver.com>
2020-11-15 17:50:26 +0900

    C 22-23

---
+++ 42seoul/C/C_Projects/열혈/chapter22/22_1.c
@@ -0,0 +1,5 @@
+struct employee{
+    char name[20];
+    char num[20];
+    int salary;
+};
---
+++ 42seoul/C/C_Projects/열혈/chapter22/add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 2;
+    int b = 2;
+    int c = a + b;
+
+    printf("%d + %d = %d\n", a, b, c);
+    return 0;
+}
